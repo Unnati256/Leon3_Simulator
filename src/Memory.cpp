fe370a78e3e499176f1b86a2eba16303d3ce47ad
UnnatiA <170010006@iitdh.ac.in>
2020-08-28 15:12:57 +0530

    changes in memory

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -10,26 +10,13 @@ Memory::Memory () {
 }
 
 int Memory::getValue (int mem_address) {
-	int temp;
-	std::string value_str;
-	for(int i = mem_address; i < mem_address + 4; i++){
-		temp = (int) memory[i];
-		value_str = value_str + std::bitset<8>(temp).to_string();
-	}
-	int value = stoi(value_str, 0, 2);
+	int value = memory[mem_address+3] | (memory[mem_address + 2] << 8) | (memory[mem_address + 1] << 16) | (memory[mem_address] << 24);
 	return value;
 }
 
 void Memory::setValue (int mem_address, int value) {
-	std::string binary = std::bitset<32>(value).to_string();
-	int k = -1;
-	std::string stemp = "";
-	for (int i = 0; i < 32; i++) {
-		stemp = stemp + binary[i];
-		if (i % 8 == 7) {
-			k++;
-			memory[mem_address + k] = static_cast<unsigned char>(std::bitset<8>(stemp).to_ulong());
-			stemp = "";
-		}
-	}
+	memory[mem_address] = (value >> 24) & 0xFF;
+    memory[mem_address + 1] = (value >> 16) & 0xFF;
+    memory[mem_address + 2] = (value >> 8) & 0xFF;
+    memory[mem_address + 3] = value & 0xFF;
 }
